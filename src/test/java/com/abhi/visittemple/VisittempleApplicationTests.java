968c86f7def9a63540821961b57d0a5b643bd8b6
Abhishek Yadav <07abhiyadav07@gmail.com>
2024-08-31 21:56:09 +0530

    inital commit

---
+++ src/test/java/com/abhi/visittemple/VisittempleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhi.visittemple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisittempleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
